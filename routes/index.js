cf8a567c618ab84da71e57528333ff5008f5bec1
kuina <kuinanein@gmail.com>
2018-07-05 01:03:16 +0900

    パスワード付与機能追加

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
+const path = require('path');
+
+const express = require('express');
+const router = express.Router();
 
 const URLSafeBase64 = require('urlsafe-base64');
 const multer = require('multer');
 const { google } = require('googleapis');
+const Minizip = require('minizip-asm.js');
+const MersenneTwister = require('mersenne-twister');
 
 const clientSecret = require('./client-secret.json').web;
+const ALNUM = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 const upload = multer();
 
@@ -40,13 +45,22 @@ function b64 (s) {
 }
 
 router.post('/upload-files', upload.array('files'), function(req, res, next) {
-  const fs = req.files.map(f => {
-    return {
-      name: f.originalname,
-      body: new Buffer(f.buffer, 'base64')
-    };
-  });
-  const auth = createGoogleAuth(req.hostname);
+  const generator = new MersenneTwister();
+  let password = '';
+  for (let i = 0; i < 13; ++i) {
+    // 出力値は[0,1) の半開区間であるはずだが念の為minをかます
+    password += ALNUM.charAt(Math.min(ALNUM.length - 1,
+      Math.floor(generator.random() * ALNUM.length)));
+  }
+
+  const mz = new Minizip();
+  for (const f of req.files) {
+    mz.append(f.originalname, f.buffer, { password })
+  }
+  const attachmentName = (req.body.fname ||
+      path.basename(req.files[0].originalname,
+        path.extname(req.files[0].originalname))) + '.zip';
+
   const boundary = '=_Kaien315114194';
   let body = `Subject: =?UTF-8?B?${b64(req.body.subject)}?=
 Content-Type: multipart/mixed; boundary="${boundary}"
@@ -55,28 +69,42 @@ Content-Type: multipart/mixed; boundary="${boundary}"
 Conent-Type: text/plain; charset="UTF-8";
 Content-Transfer-Encoding: base64
 
-${b64('こんにちは、世界！')}\n`
-  for (const f of req.files) {
-    // Content-Type: ${f.mimetype}; name="=?UTF-8?B?${b64(f.originalname)}?="
-    body += `--${boundary}
-Content-Type: ${f.mimetype}; name="=?UTF-8?B?${f.originalname}?="
+パスワードは別メールでお送りいたします。\n`;
+
+  body += `--${boundary}
+Content-Type: application/zip; name="=?UTF-8?B?${attachmentName}?="
 Content-Transfer-Encoding: base64
 
-${f.buffer.toString('base64')}\n`
-  }
-  body += `--${boundary}--\n`;
+${mz.zip().toString('base64')}
+--${boundary}--\n`;
 
   const b64ed = b64(body);
+  const b64ed2 = b64(`Subject: =?UTF-8?B?${b64('【PW】' + req.body.subject)}?=
+
+別メールにて送信した添付ファイルのパスワードは
+「${password}」
+になります。
+`);
+
+  const result = {};
+  const auth = createGoogleAuth(req.hostname);
+  const gmail = google.gmail({version: 'v1', auth})
   auth.getToken(req.body.code).then(r => {
-    const gmail = google.gmail({version: 'v1', auth})
     auth.credentials = r.tokens
     return gmail.users.drafts.create({
       userId: 'me',
       resource: { message: { raw: b64ed } }
     });
   }).then(r => {
+    result.mail1 = r.data
+    return gmail.users.drafts.create({
+      userId: 'me',
+      resource: { message: { raw: b64ed2 } }
+    });
+  }).then(r => {
+    result.mail2 = r.data
     res.set('Content-Type', 'text/plain');
-    res.send('OK\n\n' + JSON.stringify(r.data));
+    res.send('OK\n\n' + JSON.stringify(result, null, 2));
   }).catch(err => {
     console.log(err)
     res.send('エラー！: ' + err)
